d058b3de99a74a1a13917cc0ef92364ce8b6fdaf
Judy Liu <judy8liu@gmail.com>
2024-10-28 14:42:47 -0500

    combined ian's tailwind work with mealsize javascript

---
+++ src/script.js
@@ -0,0 +1,20 @@
+var currentOrder = [];
+
+let currentPrice = 0.0;
+
+// for meal size page: gets the text of each button and adds it to the array.
+const buttons = document.querySelectorAll(".sizeButton");
+
+buttons.forEach(button =>{
+    button.addEventListener("click", function() {
+        const buttonText = this.textContent.toLowerCase();
+
+        if (buttonText.includes("side")) {
+            console.log("Redirecting to sides page");
+            window.location.href = "employee-sides.html";
+        } else {
+            console.log("Redirecting to entrees page");
+            window.location.href = "employee-entrees.html";
+        }
+    });
+});
