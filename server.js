5d910e58f07b975ad8ae9afb91b6757d0d6a57ed
Alana Moses <amariemoses@gmail.com>
2024-10-29 16:06:35 -0500

    changed from client to pool to interact with database

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const {Client} = require("pg");
+const {Pool} = require("pg");
 const express = require("express");
 const path = require("path");
 
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "src")));
 app.use(express.json());
 
-const client = new Client ({
+const pool = new Pool ({
     "host": "csce-315-db.engr.tamu.edu",
     "user": "team_0p",
     "database": "team_0p_db",
@@ -44,7 +44,7 @@ start();
 async function start() {
     // await connect();
     try {
-        await client.connect()
+        await pool.connect()
         console.log("Connected to database successfully")
     } catch(e) {
         console.log(`Failed to connect to database: ${e}`);
@@ -55,7 +55,7 @@ async function addOrder(orderData) {
     try{
         // INFORMATION FOR ORDER
         // Get order id for this order
-        const orderResult = await client.query("select max(id) as max_id from orders");
+        const orderResult = await pool.query("select max(id) as max_id from orders");
         const orderID = orderResult.rows[0].max_id + 1;
         
         const currentDate = new Date();
@@ -75,14 +75,14 @@ async function addOrder(orderData) {
         const numItems = 1;
 
         // Get total price of the order
-        const priceResult = await client.query("select price from mealsizes where mealname = $1", [orderData[0]]);
+        const priceResult = await pool.query("select price from mealsizes where mealname = $1", [orderData[0]]);
         const orderPrice = priceResult.rows[0].price;
 
         console.log(`Current order data: (${orderID}, ${orderDate}, ${orderTime}, ${numItems}, ${orderPrice})`);
 
         // INFORMATION FOR ORDER ITEM
         // Get id for this item in the order
-        const orderItemsResult = await client.query("select max(id) as max_id from orderitems");
+        const orderItemsResult = await pool.query("select max(id) as max_id from orderitems");
         const orderItemsID = orderItemsResult.rows[0].max_id + 1;
 
         // Get price of this order item
@@ -91,8 +91,8 @@ async function addOrder(orderData) {
         console.log(`Current orderitems data: (${orderItemsID}, ${orderID}, ${orderItemPrice}, ${orderData[0]}, ${orderData[1]}, ${orderData[2]}, ${orderData[3]}, ${orderData[4]}})`);
 
         // Insert into orders and orderitems tables
-        await client.query("INSERT INTO orders VALUES ($1, $2, $3, $4, $5)", [orderID, orderDate, orderTime, numItems, orderPrice]);
-        await client.query("INSERT INTO orderitems VALUES ($1, $2, $3, $4, $5, $6, $7, $8)", [orderItemsID, orderID, orderItemPrice, orderData[0], orderData[1], orderData[2], orderData[3], orderData[4]]);
+        await pool.query("INSERT INTO orders VALUES ($1, $2, $3, $4, $5)", [orderID, orderDate, orderTime, numItems, orderPrice]);
+        await pool.query("INSERT INTO orderitems VALUES ($1, $2, $3, $4, $5, $6, $7, $8)", [orderItemsID, orderID, orderItemPrice, orderData[0], orderData[1], orderData[2], orderData[3], orderData[4]]);
     } catch(e) {
         console.error(`Query failed: ${e}`);
     }
@@ -109,6 +109,6 @@ async function addOrder(orderData) {
 
 // Close the client when the application exits
 process.on('exit', async () => {
-    await client.end();
+    await pool.end();
     console.log("Database client disconnected");
 });
