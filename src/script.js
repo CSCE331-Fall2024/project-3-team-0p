be6b5a29f03c2001f06ca5c8cacf81c443585015
Layla Donia <layladonia@Laylas-MacBook-Pro-9.local>
2024-11-20 13:08:32 -0600

    fixed script.js for customer

--- src/script.js
+++ src/script.js
@@ -24,20 +24,27 @@ if (storedMeal) {
 document.addEventListener("DOMContentLoaded", () => {
     const loadedWindow = window.location.pathname;
     // Loads the current order after choosing food items
-    if (loadedWindow === "/employee-review.html" || loadedWindow === "/customer-review.html" || loadedWindow === "/customer-displayMeals.html") {
+    if (loadedWindow === "/employee-review.html" || loadedWindow === "/customer-review.html" || loadedWindow === "/customer-displayMeals.html" ){
         updateOrderDisplay();
-    } else if (loadedWindow === "/employee-mealsize.html" || loadedWindow === "/customer-mealsize.html") {
+    } else if (loadedWindow === "/customer-mealsize.html"){
+        updateOrderDisplay();
         setMealSizeButtons();
-    } else if (loadedWindow === "/employee-entrees.html" || loadedWindow === "/customer-entrees.html") {
+    } else if (loadedWindow === "/customer-entrees.html") {
+        updateOrderDisplay();
         setEntreeButton();
-    } else if (loadedWindow === "/employee-sides.html" || loadedWindow === "/customer-sides.html") {
+    } else if (loadedWindow === "/customer-sides.html"){
+        updateOrderDisplay();
+        setSideButton();
+    } else if (loadedWindow === "/employee-mealsize.html") {
+        setMealSizeButtons();
+    } else if (loadedWindow === "/employee-entrees.html") {
+        setEntreeButton();
+    } else if (loadedWindow === "/employee-sides.html") {
         setSideButton();
     } else if (loadedWindow === "/customer-orderConfirmation.html") {
         displayOrderID();
     }
 });
-
-//layla 
 
 // for login page: redirect to correct page
 
@@ -426,7 +433,7 @@ async function updateOrderDisplay() {
     try {
         if (storedOrder) {
             const currentOrder = JSON.parse(storedOrder);
-            if(currentPage.includes("displayMeals")){
+            if(currentPage.includes("displayMeals") || currentPage.includes("customer-entrees") || currentPage.includes("customer-sides")){
                 let validFood = [];
                 currentOrder[currentMeal].forEach(food => {
                     if (food !== "N/A") {
@@ -452,7 +459,10 @@ async function updateOrderDisplay() {
                 gottenPrice = await getOrderPrice();
                 orderTotalElement.textContent = "Order Total: $" + gottenPrice.toFixed(2);
             }
-        } else {
+        } else if (currentPage.includes("customer-mealsize")){
+            mealDetailsElement.textContent = "No meal selected.";
+        }
+        else {
             mealDetailsElement.textContent = "No meal selected.";
             orderTotalElement.textContent = "Order Total: $0.00";
         }
@@ -461,7 +471,6 @@ async function updateOrderDisplay() {
         alert("Error displaying the order.");
     }
 }
-
 function cancelOrder() {
     console.log("cancelling order");
     const userConfirmed = confirm("Are you sure you want to proceed?");
